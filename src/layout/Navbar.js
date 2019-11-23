76b2902bab7ddc5b6bdade53d045735106946921
AnLuubk <conganwoz@gmail.com>
2019-11-23 17:59:46 +0700

    test merge

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -5,12 +5,31 @@ export default class Narbar extends React.Component {
   render() {
     return (
       <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
+        <nav className="z-depth-0" style={{ backgroundColor: '#f5ab35' }}>
+          {/* <div className="nav-wrapper white">
             <Link to="/" style={{ fontFamily: 'monospace' }} className="col s5 brand-logo center black-text">
               <i className="material-icons">code</i>
-              MERN
+              hoigiday
             </Link>
+          </div> */}
+          <div class="nav-wrapper">
+            <Link
+              to="/"
+              className="col s5 brand-logo left black-text brand-logo"
+              style={{ fontFamily: 'cursive', marginLeft: 20, fontWeight: 'bold' }}>
+              <span style={{ color: '#fff' }}>Hoigiday</span>
+            </Link>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li>
+                <a href="sass.html">Sass</a>
+              </li>
+              <li>
+                <a href="badges.html">Components</a>
+              </li>
+              <li>
+                <a href="collapsible.html">JavaScript</a>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
