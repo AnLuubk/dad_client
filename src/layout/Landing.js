4d83d99c11503d70414aa3f199f38a108efd3e2f
AnLuubk <conganwoz@gmail.com>
2019-11-23 11:07:02 +0700

    fix layout

--- src/layout/Landing.js
+++ src/layout/Landing.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 class Landing extends Component {
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
   render() {
     return (
       <div style={{ height: '75vh' }} className="container valign-wrapper">
@@ -41,4 +47,11 @@ class Landing extends Component {
     );
   }
 }
-export default Landing;
+// export default Landing;
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, {})(Landing);
